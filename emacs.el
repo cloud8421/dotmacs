a12f3d05386aa4b6b4dc42e3b6d8a6ea8519fcd5
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-24 17:08:14 +0100

    Disables standard startup screen.

--- emacs.el
+++ emacs.el
@@ -88,11 +88,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p t)
  '(custom-safe-themes
    (quote
-    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))))
+    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
+ '(inhibit-startup-screen t)
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
