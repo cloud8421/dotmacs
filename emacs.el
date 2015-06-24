1cc32d32d5f39c93aa4605d93f46c06755c28ac4
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-06-24 10:17:03 +0100

    Use monokai.

--- emacs.el
+++ emacs.el
@@ -11,6 +11,7 @@
 		      evil-leader
 		      evil-surround
 		      projectile
+		      molokai
 		      helm))
 
 ;; figure out what's missing
@@ -61,7 +62,7 @@
 (set-frame-font ui-font nil t)
 
 ;; theme
-(load-theme 'wombat)
+(load-theme 'monokai)
 
 ;; Leader shortcuts
 (evil-leader/set-key
@@ -71,3 +72,18 @@
   "b" 'eval-buffer
   "m" 'helm-M-x
   "/" 'helm-find-files)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
