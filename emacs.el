d54b28bdebc7223f040add54ba83c30e5622acfd
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-07-02 18:18:40 +0100

    Normalize font faces for org-mode.

--- emacs.el
+++ emacs.el
@@ -151,7 +151,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#FD971F" :height 1.0))))
+ '(org-level-2 ((t (:inherit variable-pitch :foreground "#A6E22E" :height 1.0))))
+ '(org-level-3 ((t (:inherit variable-pitch :foreground "#66D9EF" :height 1.0))))
+ '(org-level-4 ((t (:inherit variable-pitch :foreground "#E6DB74" :height 1.0))))
+ '(outline-1 ((t nil)))
+ '(outline-2 ((t nil)))
+ '(outline-3 ((t nil)))
+ '(outline-4 ((t nil)))
+ '(variable-pitch ((t nil))))
 
 ;; lisp
 (defun standard-lisp-modes ()
