ca5b1e8d95a9733eaf0a27cd1ff1149806478750
Claudio Ortolina <claudio@new-bamboo.co.uk>
2015-07-06 09:44:56 +0100

    Adds markdown mode.

--- emacs.el
+++ emacs.el
@@ -15,6 +15,7 @@
                       powerline powerline-evil
                       js2-mode
                       elm-mode
+                      markdown-mode
                       company
                       elixir-mode alchemist
                       magit
@@ -150,6 +151,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.0))))
  '(org-level-1 ((t (:inherit variable-pitch :foreground "#FD971F" :height 1.0))))
  '(org-level-2 ((t (:inherit variable-pitch :foreground "#A6E22E" :height 1.0))))
  '(org-level-3 ((t (:inherit variable-pitch :foreground "#66D9EF" :height 1.0))))
